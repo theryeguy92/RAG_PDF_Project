0754f6ac357d350d7e9ce328d6f9faea4c3a5f21
MFindley08 <finlynn.87@gmail.com>
2024-11-07 16:40:30 -0500

    updated bash scripts for my personal VM login

--- bash_scripts/vm_4_login.sh
+++ bash_scripts/vm_4_login.sh
@@ -3,12 +3,12 @@
 # Define the server (Just plug in the IP)
 BASTION_USER="cc"
 BASTION_IP="129.114.27.250" # Bastion IP
-BASTION_KEY="~/.ssh/F24_BASTION.pem"
+BASTION_KEY="/home/parallels/.ssh/F24_BASTION.pem"
 
 # Target server
 TARGET_USER="cc"
-TARGET_IP="192.168.5.143" # NOTE: Plug in the IP of the VM
-TARGET_KEY="~/.ssh/key_team2.pem"
+TARGET_IP="192.168.5.140" # NOTE: Plug in the IP of the VM
+TARGET_KEY="/home/parallels/.ssh/key_team2.pem"
 
 # Execute the SSH connection
 ssh -o ProxyCommand="ssh -i $BASTION_KEY -W %h:%p $BASTION_USER@$BASTION_IP" -i $TARGET_KEY $TARGET_USER@$TARGET_IP
