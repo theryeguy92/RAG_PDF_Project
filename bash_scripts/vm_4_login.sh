444f5205a34207f68399e7089c211925a776fefb
MFindley08 <finlynn.87@gmail.com>
2024-11-12 22:12:20 -0500

    updated yaml Image IP

--- bash_scripts/vm_4_login.sh
+++ bash_scripts/vm_4_login.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Define the server (Just plug in the IP)
+# Define the bastion server
 BASTION_USER="cc"
-BASTION_IP="129.114.27.250" # Bastion IP
-BASTION_KEY="/home/parallels/.ssh/F24_BASTION.pem"
+BASTION_IP="129.114.27.250"  # Bastion IP
+BASTION_KEY="/home/parallels/.ssh/F24_BASTION.pem"  # Ensure this path is correct
 
-# Target server
+# Define the target server (VM4)
 TARGET_USER="cc"
-TARGET_IP="192.168.5.128" # NOTE: Plug in the IP of the VM
-TARGET_KEY="/home/parallels/.ssh/Team2_Key.pem"
+TARGET_IP="192.168.5.128"  # The correct internal IP for VM4
+TARGET_KEY="/home/parallels/.ssh/Team2_Key.pem"  # Ensure this path is correct
 
-# Execute the SSH connection
+# Execute the SSH connection using the bastion server as a proxy
 ssh -o ProxyCommand="ssh -i $BASTION_KEY -W %h:%p $BASTION_USER@$BASTION_IP" -i $TARGET_KEY $TARGET_USER@$TARGET_IP
